5b66e3ee6144f8113b41a300529db2c7ae343072
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-09 13:30:41 -0300

    arquivo da aula de tipos de variáveis

---
+++ variaveis/tiposDeVariaveis.php
@@ -0,0 +1,13 @@
+<?php
+$nome = "Anderson"; //? string
+$idade = 90; //? inteiro, int ou integer
+$nota = 7.5; //? float
+$ligado = true; //? boolean. TRUE | FALSE
+$caracteristica = array("alegre", "feliz"); //? array
+
+echo $nome;
+$nome = 10;
+echo " ".$nome;
+
+//!em php a variável pode mudar de tipo EX: $nome é string depois eu posso mudar o tipo da variável nome para inteiro. NÃO É ACONSELHÁVEL FAZER ISSO.
+?>
