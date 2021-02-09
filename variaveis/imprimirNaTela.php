ae5245541a2950627358985fee67857117ca6ed0
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-09 13:07:09 -0300

    primeiro commit do modulo de php

---
+++ variaveis/imprimirNaTela.php
@@ -0,0 +1,14 @@
+<?php
+$nome = "Anderson";
+$sobreNome = "Jorge";
+$idade = 90;
+
+/* echo "Meu nome é: ".$nome." ".$sobreNome." e eu tenho ".$idade." anos"; */
+
+echo "Meu nome é ";
+echo $nome." ". $sobreNome;
+echo " e eu tenho ";
+echo $idade;
+echo " anos"
+
+?>
