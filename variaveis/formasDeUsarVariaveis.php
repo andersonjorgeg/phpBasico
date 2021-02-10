9ae5b5a10a29493694ab44d411fdef0f51aaee9e
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-10 11:18:05 -0300

    arquivo da aula formas de usar variável.

---
+++ variaveis/formasDeUsarVariaveis.php
@@ -0,0 +1,9 @@
+<?php
+$nome = "Anderson";
+
+/* echo "Meu nome é ".$nome; */
+
+/* echo 'Meu nome é $nome'; */ //? desse jeito vai ler tudo como string
+
+echo "meu nome é $nome"; //? pode ser feito desse jeito, mas não é aconselhável.
+?>
