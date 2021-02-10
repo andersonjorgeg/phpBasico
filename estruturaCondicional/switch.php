eaa06c1ebc3c869ca1f2c0415cd24a564bfe400f
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-10 13:57:13 -0300

    arquivos da aula switch

---
+++ estruturaCondicional/switch.php
@@ -0,0 +1,15 @@
+<?php
+$x = 0;
+
+switch($x){
+    case 0:
+    case 1: 
+    case 2:
+        echo "o x é 0, 1 ou 2";
+        break;
+    default:
+        echo "O x não é nenhum número";
+        break;
+
+}
+?>
