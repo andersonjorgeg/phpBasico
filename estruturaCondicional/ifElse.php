77f448fb0792acb19ddfbd3a252b4d69ef936bbb
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-10 11:35:57 -0300

    arquivos da aula de if/else

---
+++ estruturaCondicional/ifElse.php
@@ -0,0 +1,17 @@
+<?php 
+$nome = "Anderson";
+$idade = 35;
+if($nome == "Anderson" && $idade == 35) {
+    echo "Dados corretos";
+} else {
+    echo "Dados INCORRETO";
+} 
+
+/* $nome = "fulano";
+if($nome == "Anderson") {
+    echo "Meu nome está CORRETO!";
+} else {
+    echo "Meu nome está INCORRETO!";
+} */
+
+?>
