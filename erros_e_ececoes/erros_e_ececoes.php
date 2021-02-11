2d14304994ade972990626d622fabfc82841e2e1
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-11 20:38:20 -0300

    arquivo da aula sobre erros e eceções

---
+++ erros_e_ececoes/erros_e_ececoes.php
@@ -0,0 +1,15 @@
+<?php
+
+//?Fatal error: Uncaught Error: Call to undefined function jdskdbkjds() in C:\wamp\www\projetox\erros_e_ececoes\erros_e_ececoes.php on line 3
+/* echo jdskdbkjds(); */
+
+//?Warning: substr() expects at least 2 parameters, 0 given in C:\wamp\www\projetox\erros_e_ececoes\erros_e_ececoes.php on line 6
+/* echo substr(); */
+
+//?Warning: substr() expects at most 3 parameters, 8 given in C:\wamp\www\projetox\erros_e_ececoes\erros_e_ececoes.php on line 9
+/* echo substr(1,2,3,4,5,6,7,8); */
+
+//?Parse error: syntax error, unexpected 'rand' (T_STRING), expecting ',' or ';' in C:\wamp\www\projetox\erros_e_ececoes\erros_e_ececoes.php on line 13
+/* echo substr(1, 2)
+rand(); */
+?>
